6daf161f1013c28242533c854aff5a525a940a8d
Pavlo Antoniv <pavlivavli@gmail.com>
2023-04-08 18:18:49 +0200

    create promise

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,76 +1,3 @@
-// import flatpickr from 'flatpickr';
-// import 'flatpickr/dist/flatpickr.min.css';
-
-// const options = {
-//   enableTime: true,
-//   time_24hr: true,
-//   defaultDate: new Date(),
-//   minuteIncrement: 1,
-//   onClose(selectedDates) {
-//     console.log(selectedDates[0]);
-//   },
-// };
-
-// flatpickr('#datetime-picker', options);
-
-// const timerDays = document.querySelector('[data-days]');
-// const timerHours = document.querySelector('[data-hours]');
-// const timerMinutes = document.querySelector('[data-minutes]');
-// const timerSeconds = document.querySelector('[data-seconds]');
-// const startButton = document.querySelector('[data-start]');
-
-// let deadline;
-
-// function getTimeRemaining() {
-//   const total = Date.parse(deadline) - Date.parse(new Date());
-//   const seconds = Math.floor((total / 1000) % 60);
-//   const minutes = Math.floor((total / 1000 / 60) % 60);
-//   const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
-//   const days = Math.floor(total / (1000 * 60 * 60 * 24));
-
-//   return {
-//     total,
-//     days,
-//     hours,
-//     minutes,
-//     seconds,
-//   };
-// }
-
-// function initializeClock() {
-//   function updateClock() {
-//     const t = getTimeRemaining();
-
-//     timerDays.innerHTML = ('0' + t.days).slice(-2);
-//     timerHours.innerHTML = ('0' + t.hours).slice(-2);
-//     timerMinutes.innerHTML = ('0' + t.minutes).slice(-2);
-//     timerSeconds.innerHTML = ('0' + t.seconds).slice(-2);
-
-//     if (t.total <= 0) {
-//       clearInterval(timeinterval);
-//     }
-//   }
-
-//   updateClock();
-//   const timeinterval = setInterval(updateClock, 1000);
-// }
-
-// startButton.addEventListener('click', () => {
-//   deadline = new Date(document.querySelector('#datetime-picker').value);
-//   initializeClock();
-// });
-
-// const fp = flatpickr('#date', {
-//   // опції конфігурації календаря
-//   onClose(selectedDates, dateStr, instance) {
-//     const selectedDate = selectedDates[0];
-//     if (selectedDate < Date.now()) {
-//       window.alert('Please choose a date in the future');
-//     }
-//   },
-// });
-// і;
-
 import flatpickr from 'flatpickr';
 
 import 'flatpickr/dist/flatpickr.min.css';
@@ -148,7 +75,7 @@ flatpickr(dateTimePicker, {
       strBtn.disabled = true;
     } else {
       Notiflix.Report.success(
-        'Great 😎',
+        'Great',
         'The selected date is in the future. Click `Start` to continue',
         'Start'
       );
